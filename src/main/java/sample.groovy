b829ea9d7cf142b4462c67e978e6d30c3d39b52e
sarlak3@gmail.com <sarlak3@gmail.com>
2020-05-12 15:20:30 +0430

    create table foe path and api

--- src/main/java/sample.groovy
+++ src/main/java/sample.groovy
@@ -5,14 +5,30 @@ import groovy.sql.Sql
 
 class GroovyTest {
 
+    static def yamlFile = new File("example.yml")
     static def dbUrl = "jdbc:postgresql://localhost/dbtest"
     static def dbUser = "testuser"
     static def dbPassword = "test"
     static def dbDriver = "org.postgresql.Driver"
+    static def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
 
     static void main(args) {
-// -------------------------------- sql configuration -----------------------------------------
-        def sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
+        //sql configuration
+        dbInitialization()
+        // yaml reader
+        yamlFile.withReader { reader ->
+            def yaml = new YamlSlurper().parse(reader)
+            def json = JsonOutput.toJson(yaml)
+            println JsonOutput.prettyPrint(json)
+            def models = yaml.definitions
+            generateModel(models)
+            def paths = yaml.paths
+            generatePath(paths)
+            sql.close()
+        }
+    }
+//  sql configuration
+    static void dbInitialization() {
         sql.execute("DROP TABLE IF EXISTS model cascade;")
         sql.execute("CREATE TABLE model (model_id serial NOT NULL,name varchar(100) NOT NULL," +
                 "type varchar(100) NOT NULL,xml_name varchar(100)  NULL," +
@@ -21,61 +37,68 @@ class GroovyTest {
         sql.execute("CREATE TABLE propertiestb(propertiestb_id serial NOT NULL," +
                 "name varchar(100) NOT NULL,type varchar(100) NULL,format varchar(100) NULL," +
                 "description varchar(100) NULL,isDefault varchar(100) NULL,example varchar(100) NULL," +
-                "ref varchar(100) NULL,enum varchar(100) NULL,xml_name varchar(100) NULL, xml_wrapped varchar(100) NULL,model_id integer NOT NULL," +
+                "ref varchar(100) NULL,enum varchar(100) NULL,xml_name varchar(100) NULL, xml_wrapped varchar(100) NULL,items varchar(100) NULL ,model_id integer NOT NULL," +
                 "CONSTRAINT PK_propertiestb PRIMARY KEY ( propertiestb_id)," +
                 "CONSTRAINT FK_21 FOREIGN KEY ( model_id) REFERENCES model ( model_id));")
-        sql.execute("DROP TABLE IF EXISTS PATHS;")
-        sql.execute "create table PATHS (ID serial PRIMARY KEY, URL VARCHAR (100))"
+        sql.execute("DROP TABLE IF EXISTS path cascade;")
+        sql.execute(
+                "CREATE TABLE path(" +
+                        " path_id  serial NOT NULL," +
+                        " url     varchar(50) NOT NULL," +
+                        " CONSTRAINT PK_path PRIMARY KEY (path_id));")
+        sql.execute("DROP TABLE IF EXISTS api ;")
+        sql.execute(
+                "CREATE TABLE api (" +
+                        " api_id serial NOT NULL," +
+                        " type   varchar(50) NULL," +
+                        " tags        varchar(100) NULL," +
+                        " summary      varchar(200) NULL," +
+                        " description  varchar(200) NULL," +
+                        " operation_id varchar(100) NULL," +
+                        " consumes     varchar(100) NULL," +
+                        " produces     varchar(100) NULL," +
+                        " path_id    integer   NOT NULL," +
+                        " CONSTRAINT PK_api PRIMARY KEY (api_id)," +
+                        " CONSTRAINT FK_44 FOREIGN KEY (path_id) REFERENCES path (path_id));")
 
+    }
+// parse path data
+    static void generatePath(paths) {
+        for (path in paths) {
+            sql.executeInsert(" INSERT INTO path ( URL) VALUES (?) ", [path.key]);
+            for (item in path.getValue()) {
+                def first = sql.firstRow("SELECT path_id FROM path where path.url=" + "'" + path.key + "'")
+                int path_id = first.path_id
 
-//------------------------------ yaml reader---------------------------------------------------
-
-        yamlFile.withReader { reader ->
-            def yaml = new YamlSlurper().parse(reader)
-            def json = JsonOutput.toJson(yaml)
-            println("yaml to json")
-            println(json)
-            def models = yaml.definitions
-            println("models to json")
-            for (model in models) {
-
-                def model_json = JsonOutput.toJson(model)
-                if (model.getValue().xml != null) {
-                    sql.executeInsert(" INSERT INTO MODEL ( name , type , xml_name) VALUES (?,?,?) ", [model.key, model.getValue().type, model.getValue().xml.name]);
-                } else {
-                    sql.executeInsert(" INSERT INTO MODEL ( name , type ) VALUES (?,?) ", [model.key, model.getValue().type]);
-                }
-
-                for (property in model.value.properties) {
-                    def first = sql.firstRow("SELECT model_id FROM model where model.name=" + "'" + model.key + "'")
-                    int model_id = first.model_id
-                    if (property.getValue().xml == null) {
-                        sql.executeInsert(" INSERT INTO propertiestb (name , type  , format , description , isDefault , example , ref , enum  , model_id) VALUES (? ,?, ?,? ,?,?,?,?,?) ",
-                                [property.key, property.getValue().type, property.getValue().format, property.getValue().description, property.getValue().default, property.getValue().example,
-                                 property.getValue().ref, property.getValue().enum.toString(), model_id])
-                    } else {
-                        sql.executeInsert(" INSERT INTO propertiestb (name , type  , format , description , isDefault , example , ref , xml_name , xml_wrapped, enum  , model_id) VALUES (? ,?,?, ?,? ,? ,?,?,?,?,?) ",
-                                [property.key, property.getValue().type, property.getValue().format, property.getValue().description, property.getValue().default, property.getValue().example,
-                                 property.getValue().ref, property.getValue().xml.name, property.getValue().xml.wrapped, property.getValue().enum.toString(), model_id])
-                    }
-                }
+                sql.executeInsert(" INSERT INTO api ( type ,tags , summary ,description , operation_id ,consumes , produces , path_id) VALUES (?,? , ?,?,?,? ,? , ?) ",
+                        [item.getKey(), item.getValue().tags[0], item.getValue().summary, item.getValue().description, item.getValue().operationId, item.getValue().consumes.toString(), item.getValue().produces.toString(), path_id]);
             }
-            println("paths to json")
-            def paths = yaml.paths
-            for (path in paths) {
-                println(path.key)
 
-                sql.executeInsert(" INSERT INTO PATHS ( URL) VALUES (?) ", [path.key]);
-
-              /*  def path_json = JsonOutput.toJson(path)
-                println JsonOutput.prettyPrint(path_json)*/
-            }
-            sql.close()
         }
     }
-
-    static def yamlFile = new File("example.yml")
-
+// parse model data
+    static def generateModel(models) {
+        for (model in models) {
+            if (model.getValue().xml != null) {
+                sql.executeInsert(" INSERT INTO MODEL ( name , type , xml_name) VALUES (?,?,?) ", [model.key, model.getValue().type, model.getValue().xml.name]);
+            } else {
+                sql.executeInsert(" INSERT INTO MODEL ( name , type ) VALUES (?,?) ", [model.key, model.getValue().type]);
+            }
+            for (property in model.value.properties) {
+                def first = sql.firstRow("SELECT model_id FROM model where model.name=" + "'" + model.key + "'")
+                int model_id = first.model_id
+                if (property.getValue().xml == null) {
+                    sql.executeInsert(" INSERT INTO propertiestb (name , type  , format , description , isDefault , example , ref , enum  ,items, model_id) VALUES (? ,?, ?,? ,?,?,?,?,?,?) ",
+                            [property.key, property.getValue().type, property.getValue().format, property.getValue().description, property.getValue().default, property.getValue().example,
+                             property.getValue().$ref, property.getValue().enum.toString(), property.getValue().items.toString(), model_id])
+                } else {
+                    sql.executeInsert(" INSERT INTO propertiestb (name , type  , format , description , isDefault , example , ref , xml_name , xml_wrapped, enum ,items , model_id) VALUES (? ,?,?, ?,? ,?,? ,?,?,?,?,?) ",
+                            [property.key, property.getValue().type, property.getValue().format, property.getValue().description, property.getValue().default, property.getValue().example,
+                             property.getValue().$ref, property.getValue().xml.name, property.getValue().xml.wrapped, property.getValue().enum.toString(), property.getValue().items.toString(), model_id])
+                }
+            }
+        }
+    }
 }
 
 
