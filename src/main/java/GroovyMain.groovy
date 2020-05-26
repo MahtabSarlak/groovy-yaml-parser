12c87f4969db152348e57ca7bda7502e070254a9
sarlak3@gmail.com <sarlak3@gmail.com>
2020-05-26 08:21:29 +0430

    refactoring

--- src/main/java/GroovyMain.groovy
+++ src/main/java/GroovyMain.groovy
@@ -2,7 +2,7 @@ import groovy.yaml.YamlSlurper
 import groovy.json.JsonOutput;
 import groovy.sql.Sql
 
-class GroovyTest {
+class GroovyMain {
     static void main(args) {
         YamlParser parser = new YamlParser(Config.getSql(), "example.yml")
         parser.parse()
